62d2e5dfacb8d891bb90c6f1a36bcc788eca1c21
Lennart Kroneman <a.l.kroneman@gmail.com>
2023-04-02 23:02:21 -0500

    subscription form

--- src/services/subscription/SubscriptionErrors.ts
+++
@@ -1,9 +0,0 @@
-export class ValidationError extends Error {
-  private type: string;
-  constructor(message?: string) {
-    super(message);
-
-    this.message = message || 'ValidationError';
-    this.type = 'ValidationError';
-  }
-}
